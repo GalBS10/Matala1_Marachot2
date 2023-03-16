812f74fbc95a40d26a28340b7cf6c218f63a5144
GalBS10 <117944971+GalBS10@users.noreply.github.com>
2023-03-16 14:45:04 +0200

    first upload(all works fine)
    needs to think about more extra cases

---
+++ AdptArray.c
@@ -0,0 +1,124 @@
+#include "string.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "AdptArray.h"
+
+#define SIZE 20;
+
+typedef struct AdptArray_{
+
+int ArrSize;
+PElement* pelemArr;
+DEL_FUNC df;
+COPY_FUNC cf;
+PRINT_FUNC pf;
+
+}AdptArray , *PAdptArray;
+
+
+PAdptArray CreateAdptArray(COPY_FUNC cf, DEL_FUNC df,PRINT_FUNC pf){
+
+    PAdptArray PAarr = (PAdptArray)malloc(sizeof(AdptArray));
+    if(!PAarr){ return NULL; }
+
+    PAarr->ArrSize =0;
+    PAarr->cf = cf;
+    PAarr->df = df;
+    PAarr->pf = pf;
+    PAarr->pelemArr = NULL;
+    
+    return PAarr;
+
+}
+
+void DeleteAdptArray(PAdptArray PAarr){
+
+    if(!PAarr){
+        exit(1);
+    }
+
+    for(int i=0; i<PAarr->ArrSize;i++){
+
+        if(PAarr->pelemArr[i] != NULL){
+
+            (*PAarr->df)(PAarr->pelemArr[i]);  //deleting the element with the suitable del func.
+
+        }
+
+    }
+
+    free(PAarr->pelemArr);
+    free(PAarr);
+
+}
+
+Result SetAdptArrayAt(PAdptArray PAarr, int index, PElement pelem){
+
+    if(PAarr == NULL || pelem == NULL){
+        return FAIL;
+    }
+    
+    if(index >= PAarr->ArrSize){ // we want tp declare on a new Array
+
+        PElement* newArr = (PElement*)calloc((index+1),sizeof(PElement));
+        if(!newArr){return FAIL;}
+
+        for(int i=0; i<PAarr->ArrSize; i++){
+            newArr[i] = PAarr->pelemArr[i];
+        }
+
+        free(PAarr->pelemArr);
+        PAarr->pelemArr = newArr;
+    }
+
+    if(PAarr->pelemArr[index]!=NULL){
+
+        (*PAarr->df)(PAarr->pelemArr[index]);  //deleting the element with the suitable del func.
+
+    }
+
+    PAarr->pelemArr[index] = (*PAarr->cf)(pelem);
+    if(index>PAarr->ArrSize){
+        PAarr->ArrSize = index+1;
+    }  //else stay the same size as befor.
+    
+    return SUCCESS;
+
+}
+
+PElement GetAdptArrayAt(PAdptArray PAarr, int index){
+
+if(!PAarr){
+    //exit(1);
+    return NULL;
+}
+
+if(PAarr->pelemArr[index] == NULL){
+    
+    return NULL;
+
+}
+
+return (*PAarr->cf)(PAarr->pelemArr[index]);
+
+}
+
+int GetAdptArraySize(PAdptArray PAarr){
+    if(!PAarr){
+        return -1;
+    }
+    return PAarr->ArrSize;
+}
+
+void PrintDB(PAdptArray PAarr){
+    if(!PAarr){printf("PAarr is null");}
+    printf("PAarr : [\n");
+    for(int i=0; i<PAarr->ArrSize; i++){
+        
+        if(PAarr->pelemArr[i]!=NULL){
+            printf("%d. ",i);
+            (*PAarr->pf)(PAarr->pelemArr[i]);
+        }
+    }
+     printf("]\n");
+}
